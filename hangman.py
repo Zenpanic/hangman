3e3236455e99688f052616e43484b3dace2cec9a
Zenpanic <sebastien.belmo@protonmail.com>
2020-08-25 21:57:03 +0200

    comments added

--- hangman.py
+++ hangman.py
@@ -6,30 +6,36 @@ word_list = ["cookie", "telescope", "robot", "couch", "australia", "zebra", "gui
 
 
 def format_answer(answer):
+
+    # Returns the answer as a nice readale string.
+
     return "".join(answer)
 
 
 def update_answer(answer, word, letter):
 
-    if letter in word:
-        if letter not in answer:
-            for index, item in enumerate(word):
-                if item == letter:
-                    answer[index] = word[index]
+    # We loop through the word to know the locations of the letter, and assign the same locations in the answer.
+
+    for index, item in enumerate(word):
+        if item == letter:
+            answer[index] = word[index]
 
     return format_answer(answer)
 
 
 def prompt_guess():
 
-    regex = r"^[a-zA-Z]$"
+    regex = r"^[a-z]$"  # The pattern locks on a single alphabetical character.
 
     lucky_number = random.randint(0, len(word_list))
     word = word_list[lucky_number]
     steps = 0
     door = "*"
+
+    # We put as many placeholders as there are letters in the player's guess.
     answer = [door for i in range(len(word))]
 
+    # It loops as long as there are placeholders in the temporary answer.
     while door in answer:
         letter = input("Guess a letter! : ")
         if re.search(regex, letter):
